6ada70a1a17adbb95f883d471b70f69abc0a56aa
Chris Minnick <chris@minnick.com>
2022-08-05 15:01:03 -0700

    book 3

---
+++ Book3/Chapter02/Listing0201.js
@@ -0,0 +1,11 @@
+function SearchForm() {
+  return (
+    <form>
+      <label htmlFor="searchterm">
+        Search For:
+        <input type="text" id="searchTerm" />
+      </label>
+      <button>Search</button>
+    </form>
+  );
+}
---
+++ Book3/react-test-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Message from './components/Chapter02/Listing0203';
+
+function App() {
+  return (
+    <div className="App">
+      <Message messageType="error" message="this is a test" />
+    </div>
+  );
+}
+
+export default App;
