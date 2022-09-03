8d705f0ce591ab898a2dab5d55f1cd9d0869f5df
Chris Minnick <chris@minnick.com>
2022-09-03 07:15:14 -0700

    new listings

---
+++ Book4/Chapter05/vue-project/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Listing0504 from './components/Listing0504.vue';
+</script>
+
+<template>
+  <Listing0504 />
+</template>
---
+++ Book4/Chapter05/vue-project/src/components/Listing0503.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+
+function submitData(event) {
+  event.preventDefault();
+  console.log(event.target.firstName.value);
+}
+</script>
+
+<template>
+  <div>
+    <form @submit="submitData">
+      <input id="firstName" />
+      <input id="lastName" />
+      <input type="submit" value="Submit form" />
+    </form>
+  </div>
+</template>
