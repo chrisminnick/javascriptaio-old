b55e107afca3db0d28b162546d3cfb1a4b167638
Chris Minnick <chris@minnick.com>
2022-09-12 03:42:27 -0700

    add demo app

---
+++ Book5/Chapter02/my-svelte-app/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+let posts = [];
+let newPost = '';
+
+function addPost(){
+  posts = [...posts,newPost];
+}
+
+</script>
+
+<main>
+  <h1>Loner</h1>
+  <h2>Social media without the sharing</h2>
+  <label>Talk to yourself: <input bind:value={newPost} type="text"></label>
+  <button on:click={addPost}>Post it!</button>
+  <div>
+      {#each posts as post}
+        <div>{post}</div>
+      {/each}
+  </div>
+</main>
+this is just some text
+number
+
+<style>
+  
+</style>
---
+++ Book5/Chapter02/my-svelte-app/src/lib/BlogPost.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let blogPost = "Your post goes here.";
+    export let blogTitle = "Your title goes here.";
+    export let published = false;
+  </script>
+  
+  <article>
+    {#if published}
+    <h1>{blogTitle}</h1>
+    <p>{blogPost}</p>
+    {:else } 
+    <h1>This post is not yet live</h1>
+    {/if}
+  </article>
+  
