4cfaa2d56b3ceea39133267714017c41d077fcd4
Chris Minnick <chris@minnick.com>
2022-09-15 09:01:40 -0700

    working on chapter 5

--- Book5/Chapter05/my-svelte-app/src/App.svelte
+++ Book5/Chapter05/my-svelte-app/src/App.svelte
@@ -1,11 +1,5 @@
 <script>
-  import ScrollToPosition from './lib/ScrollToPosition.svelte'
-  
-  </script>
-  <ScrollToPosition />
-  <style>
-    /* Style error messages nested in this component's divs */
-    div :global(.error){
-      color: red;
-    }
-  </style>
+  import StockTicker from './lib/StockTickerAwait.svelte';
+</script>
+
+<StockTicker />
