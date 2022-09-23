1136107cd2dc56c14aa1850055e6b161cf85774d
Chris Minnick <chris@minnick.com>
2022-09-23 13:40:37 -0700

    renaming directories

--- Book6/automated-build-template-react/src/App.js
+++ Book6/automated-build-template-react/src/App.js
@@ -1,21 +1,97 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 
-function Obstacle({ obstaclePosition }) {
-  return <div className="obstacle" style={obstaclePosition}></div>;
+function App() {
+  useEffect(() => {
+    document.addEventListener('keydown', moveBall);
+    return () => {
+      document.removeEventListener('keydown', moveBall);
+    };
+  }, []);
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const numberOfObstacles = 100;
+  const moveBall = (e) => {
+    switch (e.key) {
+      case 'ArrowUp':
+        setPosition((prev) => {
+          return { x: prev.x, y: prev.y - 1 };
+        });
+        break;
+      case 'ArrowDown':
+        setPosition((prev) => {
+          return { x: prev.x, y: prev.y + 1 };
+        });
+        break;
+      case 'ArrowLeft':
+        setPosition((prev) => {
+          return { x: prev.x - 1, y: prev.y };
+        });
+        break;
+      case 'ArrowRight':
+        setPosition((prev) => {
+          return { x: prev.x + 1, y: prev.y };
+        });
+        break;
+    }
+  };
+
+  useEffect(() => {
+    const ball = document.getElementById('ball');
+    const ballPosition = ball.getBoundingClientRect();
+    const obstacles = document.getElementsByClassName('obstacle');
+    detectEdgeCollision(ballPosition);
+    for (let i = 0; i < obstacles.length; i++) {
+      const obstaclePosition = obstacles[i].getBoundingClientRect();
+      if (
+        ballPosition.x < obstaclePosition.x + obstaclePosition.width &&
+        ballPosition.x + ballPosition.width > obstaclePosition.x &&
+        ballPosition.y < obstaclePosition.y + obstaclePosition.height &&
+        ballPosition.y + ballPosition.height > obstaclePosition.y
+      ) {
+        alert('Game Over');
+        setPosition({ x: 0, y: 0 });
+      }
+    }
+  }, [position]);
+  function detectEdgeCollision() {
+    const ball = document.getElementById('ball');
+    const ballPosition = ball.getBoundingClientRect();
+    if (ballPosition.x < 0) {
+      setPosition((prev) => {
+        return { x: prev.x + 1, y: prev.y };
+      });
+    }
+    if (ballPosition.x + ballPosition.width > window.innerWidth) {
+      setPosition((prev) => {
+        return { x: prev.x - 1, y: prev.y };
+      });
+    }
+    if (ballPosition.y < 0) {
+      setPosition((prev) => {
+        return { x: prev.x, y: prev.y + 1 };
+      });
+    }
+    if (ballPosition.y + ballPosition.height > window.innerHeight) {
+      setPosition((prev) => {
+        return { x: prev.x, y: prev.y - 1 };
+      });
+    }
+  }
+  return (
+    <Map numberOfObstacles={numberOfObstacles}>
+      <Ball position={position} />
+    </Map>
+  );
 }
 
-function Map({ numberOfObstacles, children }) {
+function Map({ children, numberOfObstacles }) {
   const [obstacles, setObstacles] = useState([]);
+
   useEffect(() => {
     const map = generateMap(numberOfObstacles);
     setObstacles(map);
   }, []);
-  const generateRandomPosition = () => {
-    const x = Math.floor(Math.random() * 100) + 'vw';
-    const y = Math.floor(Math.random() * 100) + 'vh';
-    return { left: x, top: y };
-  };
+
   const obstaclePositions = Array(numberOfObstacles)
     .fill()
     .map(() => {
@@ -23,6 +99,7 @@ function Map({ numberOfObstacles, children }) {
       const y = Math.floor(Math.random() * 100) + 'vh';
       return { left: x, top: y };
     });
+
   const generateMap = (numberOfObstacles) => {
     const obstacles = [];
     for (let i = 0; i < numberOfObstacles; i++) {
@@ -32,11 +109,12 @@ function Map({ numberOfObstacles, children }) {
     }
     return obstacles;
   };
+
   return (
-    <div className="map">
+    <>
       {obstacles}
       {children}
-    </div>
+    </>
   );
 }
 
@@ -54,42 +132,8 @@ function Ball({ position }) {
   );
 }
 
-export default function App() {
-  useEffect(() => {
-    document.addEventListener('keydown', moveBall);
-    return () => {
-      document.removeEventListener('keydown', moveBall);
-    };
-  }, []);
-  const numberOfObstacles = 100;
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const moveBall = (e) => {
-    switch (e.key) {
-      case 'ArrowUp':
-        setPosition((prev) => {
-          return { left: prev.x, top: prev.y - 1 };
-        });
-        break;
-      case 'ArrowDown':
-        setPosition((prev) => {
-          return { left: prev.x, top: prev.y + 1 };
-        });
-        break;
-      case 'ArrowLeft':
-        setPosition((prev) => {
-          return { left: prev.x - 1, top: prev.y };
-        });
-        break;
-      case 'ArrowRight':
-        setPosition((prev) => {
-          return { left: prev.x + 1, top: prev.y };
-        });
-        break;
-    }
-  };
-  return (
-    <Map numberOfObstacles={numberOfObstacles}>
-      <Ball position={position} />
-    </Map>
-  );
+function Obstacle({ obstaclePosition }) {
+  return <div className="obstacle" style={obstaclePosition}></div>;
 }
+
+export default App;
