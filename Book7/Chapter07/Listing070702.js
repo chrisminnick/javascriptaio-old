43cf4e0e4f0747797c31a822927812e134d05d05
Chris Minnick <chris@minnick.com>
2022-12-04 03:17:40 -0800

    working on book 7 code

--- Book7/Chapter07/Listing070702.js
+++ Book7/Chapter07/Listing070702.js
@@ -2,13 +2,4 @@ async function getData() {
   return await fetch('https://nothinghere');
 }
 
-async function main() {
-  try {
-    const response = await getData();
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-main();
+getData();
